c37ae2fb4cfa1e5f7f2e5a4b211f2040bbc711e9
eamonn1 <eamonnojbrien1@gmail.com>
2020-10-05 23:02:15 +0100

    typo fix

--- RCTbalance_slimline_code/app.R
+++ RCTbalance_slimline_code/app.R
@@ -147,7 +147,7 @@
                 covariates not related to the outcome, collinear or correlated covariates related to the outcome and imbalanced covariates both of prognostic value and unrelated to the outcome.
                 As the variance of the response increases with more covariates in the simulation, it is advisable to limit the number of covariates, the default is 3. 
                 As the number of simulations to get smooth curves is high, the application may time out before simulations complete. Therefore take the code and run on your own machine. 
-                There are also four tabs presenting example results all using many simulations.
+                There is also a tab allowing loading of pre run simulations.
                 Note, the prognostic strength of treatment may be small compared with patient characteristics,
                 such as age as in the GUSTO-1 trial. This phenomenon is observed in many prognostic evaluations of RCTs:
 treatment has a 'statistically significant' impact on outcome, but its relevance is small
